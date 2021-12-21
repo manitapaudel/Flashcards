8480c9e2bfe927627a9ade9b7ee14a3f1d409c49
Manitajs <manita.paudel@ombryo.com>
2021-12-21 13:53:33 +0545

    Add export to Button component, add character entity for apostrophe in card component

--- src/components/atoms/Card/index.tsx
+++ src/components/atoms/Card/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./card.module.scss";
 
 const Card = () => {
-  return <div className={styles.card}>I'm a Card.</div>;
+  return <div className={styles.card}>I&apos;m a Card.</div>;
 };
 
 export default Card;
